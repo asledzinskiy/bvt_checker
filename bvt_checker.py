af9167f810a275283b077235ad7a2719109202fa
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-03-01 00:19:19 +0200

    Open test_all from menu
    - add possibility to open test_all from the menu

--- bvt_checker.py
+++ bvt_checker.py
@@ -14,7 +14,7 @@ import sys
 
 
 def get_last_build():
-    req = urllib2.Request(settings.BVT_JOB)
+    req = urllib2.Request(settings.BVT_JOB + '/api/json')
     opener = urllib2.build_opener(urllib2.HTTPHandler)
     s = opener.open(req).read()
     opener.close()
@@ -22,10 +22,13 @@ def get_last_build():
     build_numbers = [build["number"] for build in job_data["builds"]]
     return max(build_numbers)
 
-def bvt_health():
+def get_latest_job_url():
     last_bvt = get_last_build()
-    test_job_url = settings.BVT_TEST_JOB.format(settings.RELEASE, last_bvt)
-    req = urllib2.Request(test_job_url)
+    return '{0}/{1}'.format(settings.BVT_JOB, last_bvt)
+
+def bvt_health():
+    test_job_url = get_latest_job_url()
+    req = urllib2.Request(test_job_url + '/api/json')
     opener = urllib2.build_opener(urllib2.HTTPHandler)
     s = opener.open(req).read()
     opener.close()
@@ -41,7 +44,10 @@ def is_bvt_ok():
 class SystemTray(object):
 
     def __init__(self):
-        self.indicator = appindicator.Indicator.new(settings.APPINDICATOR_ID, os.path.abspath('green.png'), appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indicator = appindicator.Indicator.new(
+            settings.APPINDICATOR_ID,
+            os.path.abspath('green.png'),
+            appindicator.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.indicator.set_menu(self.build_menu({}))
         self.set_icon()
@@ -54,6 +60,11 @@ class SystemTray(object):
             item_review = gtk.MenuItem(active_reviews[review])
             item_review.connect('activate', self.open_url, review)
             menu.append(item_review)
+
+        item_test = gtk.MenuItem("Open results for '{0}'".format(settings.RELEASE))
+        item_test.connect('activate', self.open_url, get_latest_job_url())
+        menu.append(item_test)
+
         item_quit = gtk.MenuItem('Quit')
         item_quit.connect('activate', self.quit)
         menu.append(item_quit)
@@ -77,7 +88,10 @@ class SystemTray(object):
         return 'green.png'
 
     def set_icon(self):
-        active_reviews = gerrit_client.get_not_reviewed_patches(settings.gerrit_account_name, settings.reviews_url, settings.SKIP_LOWER_THAN)
+        active_reviews = gerrit_client.get_not_reviewed_patches(
+            settings.gerrit_account_name,
+            settings.reviews_url,
+            settings.SKIP_LOWER_THAN)
         self.indicator.set_menu(self.build_menu(active_reviews))
         icon = self.choose_icon(active_reviews)
         self.indicator.set_icon(os.path.abspath(icon))
--- settings.py
+++ settings.py
@@ -2,7 +2,6 @@ gerrit_account_name = 'asledzinskiy'
 reviews_url = 'https://review.openstack.org/changes/?q=is:open+owner:{0}&q=is:open+reviewer:{0}+-owner:{0}&q=is:closed+owner:{0}+limit:5&o=LABELS'.format(gerrit_account_name)
 APPINDICATOR_ID = 'myappindicator'
 RELEASE = '9.0-mitaka'
-BVT_JOB = 'https://product-ci.infra.mirantis.net/job/{}.test_all/api/json'.format(RELEASE)
-BVT_TEST_JOB = 'https://product-ci.infra.mirantis.net/job/{0}.test_all/{1}/api/json'
+BVT_JOB = 'https://product-ci.infra.mirantis.net/job/{}.test_all/'.format(RELEASE)
 POLLING_INTERVAL = 300
 SKIP_LOWER_THAN = 0  # -2:  show all reviews;  -1: skip CodeReview-2; 0: skip any review with -1 or -2
